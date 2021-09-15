fc267dd8b24525241943c0ff3bcdf87bf16e041d
Toshe <m56@euroblaze.de>
2021-09-15 21:08:35 +0200

    user registration

---
+++ test.py
@@ -0,0 +1,26 @@
+# values promenlivi
+
+# reference promenili
+#
+# b = 10  # 1113 -> 10
+#
+# a = [1,2]  # 1114 -> [1, 1115], 1115 -> [2, None],
+#
+# # a = 1116
+# #  1116 -> 1114
+#
+# print(a)
+#
+# # [1, 2]
+#
+# def test_func():
+#     print("Hello")
+#
+#
+# # test_func -> 2222
+# # abc -> 2222
+#
+# abc = test_func
+#
+# abc()
+
